208a39c2e2cd92bdf71485787fc5974b5f24414a
alexkrtc1 <alexkrtc1@gmail.com>
2022-07-02 20:32:04 +0300

    hk_personal_signature

---
+++ hk_personal_signature/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'Personal signature and seal on pdf reports - documents',
+    'version': '1.0',
+
+    'author': 'Hot Key',
+    'website': 'https://hotkey.com/',
+    'license': 'OPL-1',
+    'category': 'Localization',
+
+    'depends': ['base',],
+    'data': [
+        'report/invoice_inherit.xml'
+    ],
+    'installable': True,
+    'demo': [
+
+    ],
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+
+}
---
+++ hk_personal_signature/models/company.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import fields, models
+
+class Company(models.Model):
+    _inherit = 'res.company'
+
+    # Add a new column to the res.company model, by default partners are not
+    # sign and stamp image
+    sign_image = fields.Binary(string="Personal signature")
+    stamp_image = fields.Binary(string="Stamp of company")
+
+class Users(models.Model):
+    _inherit = 'res.users'
+
+    # Add a new column to the res.company model, by default partners are not
+    # sign and stamp image
+    sign_image = fields.Binary(string="Personal signature")
+    stamp_image = fields.Binary(string="Stamp of company")
+
