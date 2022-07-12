3911bad78556e0f2e27830ca80e6b4d0ab7be2ee
alexkrtc1 <alexkrtc1@gmail.com>
2022-07-12 10:27:56 +0300

    refactoring app

--- hk_personal_signature/models/users_company.py
+++ hk_personal_signature/models/users_company.py
@@ -4,16 +4,19 @@ from odoo import fields, models
 class Company(models.Model):
     _inherit = 'res.company'
 
-    # Add a new column to the res.company model, by default partners are not
-    # sign  image
+    # Add a new column to the res.company model,
+    # stamp  image
 
     stamp_image = fields.Binary(string="Stamp of company")
 
 class Users(models.Model):
     _inherit = 'res.users'
 
-    # Add a new column to the res.company model, by default partners are not
-    # sign and stamp image
+    # Add a new column to the res.users model,
+    # sign image
     sign_image = fields.Binary(string="Personal signature")
+
+
+
 
 
